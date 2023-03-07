17bd2ddc2413d4f42e844ef2602ef7d7d6264af3
daniellykammers <dany.kammers@hotmail.com>
2023-03-06 22:10:47 -0300

    feat(promises): resolved execises

--- exemplos/promise/index.js
+++ exemplos/promise/index.js
@@ -9,25 +9,87 @@ function createBankList(banks) {
 
 // ---------------------------
 // Criando uma promessa
-const successPromise = () => {};
+const myPromise = new Promise((resolve, reject) => {
+  const turma = 'Itacorubi';
 
-// ---------------------------
-// Criando Promise com reject
-const promisseError = () => {};
+  if (turma === 'Itacorubi') {
+    resolve('Você faz parte da turma Itacorubi');
+  } else {
+    reject('Infelizmente você não faz parte da turma Itacorubi');
+  }
+});
+
+const successPromise = () => {
+  myPromise
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+};
+
+// Promise com delay
+const lazyPromise = new Promise((resolve, reject) => {
+  setTimeout(() => resolve('Lazy Promise'), 2000);
+});
+
+// Promise sem delay
+const fastPromise = new Promise((resolve, reject) => {
+  setTimeout(() => resolve('Fast Promise'), 1000);
+});
+
+// Promise retorna nome
+const namePromise = new Promise((resolve, reject) => {
+  resolve('Sérgio');
+});
 
 // Encadeando then's
-const responseToJson = (cep) => {};
+const responseToJson = (cep) => {
+  return fetch(`https://viacep.com.br/ws/${cep}/json`)
+    .then((res) => res.json())
+    .then((data) => data);
+};
 
 // ---------------------------
-// Método catch
-const fetchCep = (cep) => {};
+// Método fetch
+const fetchCep = (cep) => {
+  return fetch(`https://viacep.com.br/ws/${cep}/json`)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => err);
+};
 
-function getBanksInBrasil() {}
+function fetchBanksInBrazil() {
+  return fetch('https://brasilapi.com.br/api/banks/v1')
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+}
 
-function getCep() {}
+function getBanksInBrasil() {
+  fetchBanksInBrazil()
+    .then((data) => createBankList(data))
+    .catch((err) => console.error(err));
+}
 
-function errorPromise() {}
+function getCep() {
+  fetchCep('88101420')
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+}
 
-function promiseAll() {}
+function delayPromise() {
+  lazyPromise.then((res) => console.log(res, 'Concluí'));
 
-function promiseRace() {}
+  console.log('vou ser chamado primeiro');
+}
+
+function promiseAll() {
+  Promise.all([myPromise, lazyPromise])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err))
+    .finally(() => console.log('Finalizei a promessa'));
+}
+
+function promiseRace() {
+  Promise.race([fastPromise, lazyPromise])
+    .then((res) => console.log('O vencedor é:', res))
+    .catch((err) => console.error(err))
+    .finally(() => console.log('Finalizei a promessa'));
+}
